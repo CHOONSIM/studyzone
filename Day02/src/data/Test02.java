9f2242cf4323e4757d8979ab6d0556ff5d26e436
CHOONSIM <yoyyo9402@gmail.com>
2022-12-29 13:40:42 +0900

    영화관 물제풀이

---
+++ Day02/src/data/Test02.java
@@ -0,0 +1,13 @@
+package data;
+import java.lang.*;
+
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("성인 16000");
+		System.out.println(16000 * 5);
+		
+		System.out.println("성인 16000 청소년 10000");
+		System.out.println(16000 * 2 + 10000 * 3);
+	}
+
+}
