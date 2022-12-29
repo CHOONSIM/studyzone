cab00526527fdcac38c2cab516b587d0487ebcd6
CHOONSIM <yoyyo9402@gmail.com>
2022-12-29 12:44:59 +0900

    코드추가

---
+++ Day02/src/KH.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+public class KH {
+	public static void main(String[] args) {
+		System.out.println("Spring framework & 클라우드 융합 웹 개발자 양성과정");
+		System.out.println("프로그래밍 언어 응용");
+		System.out.println("강사명 : 황인빈");
+		
+		System.out.println("2022-12-29");  //"글자"
+		System.out.println(2022-12-29);    //숫자
+		
+		System.out.println("010-4890-7421");
+		System.out.println(010-4890-7421);
+	}
+
+}
