77a66f4af7ab7e44feb9ea29e8f74f8b8bb3898b
CHOONSIM <yoyyo9402@gmail.com>
2022-12-29 14:39:22 +0900

    영화관 문제풀이

--- Day02/src/data/Test02.java
+++ Day02/src/data/Test02.java
@@ -1,13 +1,12 @@
 package data;
 import java.lang.*;
-
 public class Test02 {
 	public static void main(String[] args) {
-		System.out.println("성인 16000");
+		
+		//1인당 16000원일 때 5명의 요금은 얼마?
+		
+		//System.out.println(80000);
 		System.out.println(16000 * 5);
 		
-		System.out.println("성인 16000 청소년 10000");
-		System.out.println(16000 * 2 + 10000 * 3);
 	}
-
 }
--- Day02/src/data/Test03.java
+++ Day02/src/data/Test03.java
@@ -1,10 +1,14 @@
+
 package data;
+import java.lang.*;
 
 public class Test03 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("성인 16000");
+		System.out.println(16000 * 5);
+		
+		System.out.println("성인 16000 청소년 10000");
+		System.out.println(16000 * 2 + 10000 * 3);
 	}
 
 }
