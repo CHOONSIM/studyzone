d3558886ea3b7f14b0876b323b21e2e66b4b89ad
CHOONSIM <yoyyo9402@gmail.com>
2022-12-29 16:57:59 +0900

    편의점 1+1 문제 풀이

---
+++ Day02/src/data/Test08.java
@@ -0,0 +1,44 @@
+package data;
+import java.lang.*;
+
+public class Test08 {
+	public static void main(String[] args) {
+		
+		//1+1 행사상품 구매금액 계산
+		
+		//입력
+		int count = 9;
+		
+		//계산
+		int free = count / 2;
+		
+		//유료가격 int fare = (count+1) / 2;
+		int fare = count - free;
+		
+
+		//출력
+		System.out.println(free);
+		
+		 
+		
+		
+		
+		
+		//int kib = 1;
+		//int kibSub = 1;
+		//int kibCount = kib + kibSub;
+		
+		//int kibTotal = kibCount * 9; 
+		//int kibBuy = kib * 9;
+		//int kibTotalService = kibTotal - kibBuy;
+		
+		//int kibPrice = 1200;
+		
+		//int kibTotalPrice = kibBuy * kibPrice;
+		
+		//System.out.println(kibTotalService);
+		//System.out.println(kibTotalPrice + " 원");
+		
+	}
+
+}
