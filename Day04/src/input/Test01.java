3e670f9c3906b1a84ae4257c370e74ed14aa6d68
CHOONSIM <yoyyo9402@gmail.com>
2023-01-02 17:31:25 +0900

    입력 예제(Scanner)

---
+++ Day04/src/input/Test01.java
@@ -0,0 +1,14 @@
+package input;
+import java.lang.*;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//표준 입력
+		//- 표준 입력 통로(System.in)에서 데이터를 가져오는 행위
+		//- 내용이 정해져있는 출력과 달리 시기와 내용이 불확실하다.
+		//- ASCII 코드값을 읽어온다.
+
+	}
+
+}
