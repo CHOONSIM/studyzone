78204d58201fd45ef122cf660ea54c74e3e5e584
CHOONSIM <yoyyo9402@gmail.com>
2023-01-03 14:22:18 +0900

    문제 풀이(369)

---
+++ Day05/src/loop/Test04.java
@@ -0,0 +1,24 @@
+package loop;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		//다음 요구사항에 맞는 값을 계산하여 출력
+		//두 자리 정수 중에서 369게임을 한다면 박수를 쳐야하는 숫자를 출력
+		
+		//내 풀이
+		
+		for(int i=3; i<=99; i++) {
+			int one = i % 10;
+			int ten = i / 10;
+			
+			if(one % 3 == 0 ) {
+				System.out.println("박수 : "+i);
+			}           
+			
+			
+			}
+
+	}
+
+}
---
+++ Day05/src/loop/Test4_1.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Test4_1 {
+
+	public static void main(String[] args) {
+		//다음 요구사항에 맞는 값을 계산하여 출력
+		//두 자리 정수 중에서 369게임을 한다면 박수를 쳐야하는 숫자를 출력
+		
+		
+		// 문제 풀이
+		
+
+
+	}
+
+}
