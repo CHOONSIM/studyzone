d9f04aa2edfb01ba9dd258e7a6135c99f5aab1ff
CHOONSIM <yoyyo9402@gmail.com>
2023-01-03 14:24:23 +0900

    문제 풀이(타이머)

--- Day05/src/loop/Test05_1.java
+++ Day05/src/loop/Test05_1.java
@@ -11,6 +11,19 @@ public class Test05_1 {
 		//초 입력 :30
 		
 		//문제 풀이-1
+		int minute = 2;
+		int second = 30;
+		
+		int time = minute * 60 + second;
+		System.out.println("time = " + time);
+		
+		for(int i=time; i>0; i--) {
+			int min = i / 60;
+			int sec = i % 60;
+			System.out.println(min+"분 "+sec+"초 후 알람이 울립니다");
+		}
+		
+		System.out.println("시간이 다 되었습니다!");
 
 	}
 
--- Day05/src/loop/Test05_3.java
+++ Day05/src/loop/Test05_3.java
@@ -1,5 +1,7 @@
 package loop;
 
+import java.util.Scanner;
+
 public class Test05_3 {
 
 	public static void main(String[] args) {
@@ -11,7 +13,30 @@ public class Test05_3 {
 		//초 입력 :30
 		
 		//문제 풀이-3
+        Scanner sc = new Scanner(System.in);
 		
+		System.out.println("분 입력");
+		int minute = sc.nextInt();
+		
+		System.out.println("초 입력");
+		int second = sc.nextInt();
+		
+		sc.close();
+		
+		
+		int time = minute * 60 + second;
+		//System.out.println("time = " + time);
+		
+		for(int i=time; i>0; i--) {
+			int min = i / 60;
+			int sec = i % 60;
+			System.out.println(min+"분 "+sec+"초 후 알람이 울립니다");
+			
+			// 1초간 중지시킨다.(단위 : 밀리세컨드)
+			// Thread.sleep(1000);
+		}
+		
+		System.out.println("시간이 다 되었습니다!");
 		
 
 	}
