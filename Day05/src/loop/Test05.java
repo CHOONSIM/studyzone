38de86a194f893d9218b6eace101018c984e34a4
CHOONSIM <yoyyo9402@gmail.com>
2023-01-04 09:17:27 +0900

    문제 풀이 (알람)

--- Day05/src/loop/Test05.java
+++ Day05/src/loop/Test05.java
@@ -1,17 +1,30 @@
 package loop;
 
+import java.util.Scanner;
+
 public class Test05 {
-
 	public static void main(String[] args) {
-		//사용자에게 '분','초'를 입력받는다
-		//입력받은 시간부터 0분 0초까지 반복적으로 1초씩 감소하도록 출력
-		//모든 출력을 마치고 '시간이 다 되었습니다' 출력
 		
-		//분 입력 :2
-		//초 입력 :30
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("분을 입력하세요.");
+		int minute = sc.nextInt();
+		System.out.println("초를 입력하세요.");
+		int second = sc.nextInt();
 		
-
+		sc.close();
+		
+		int total = minute*60+second;
+		
+		for(int i=total; i>0; i-- ) {
+			int min = i / 60;
+			int sec = i % 60;
+			System.out.println(min +"분" + sec + "초 후 알람이 울립니다.");
+			
+			//1초간 중지시킨다.(단위 : 밀리세컨드)
+			// Thread.sleep(1000);
+		}
+		System.out.println("시간이 다 되었습니다!");
 	}
 
 }
