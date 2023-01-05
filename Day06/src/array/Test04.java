d622c4f2827f6c9e5986d20d9c8bf39a7ee57202
CHOONSIM <yoyyo9402@gmail.com>
2023-01-05 17:58:33 +0900

    과제풀이

--- Day06/src/array/Test04.java
+++ Day06/src/array/Test04.java
@@ -49,7 +49,7 @@ public class Test04 {
 //		성적 우수자(90점 이상) 인원수를 출력
 		int cnt = 0;
 		for(int i=0; i<data.length; i++) {
-			if(data[i] > 90) {
+			if(data[i] >= 90) {
 				cnt++;
 			}
 		}
@@ -60,15 +60,43 @@ public class Test04 {
 		
 		
 //		만약 이 반에 새로 들어온 학생이 80점이면 몇 등인지 계산하여 출력
-		int cnt1 = 0;
+		int cnt1 = 1; //등수는 1등부터 (등수 = 1+나보다 점수가 높은 인원수)
 		int stu = 80;
 		int totalStudents = data.length+1; 
 		for(int i=0; i<data.length; i++) {
-			if(stu>=data[i]) {
+			if(stu<data[i]) {
 				cnt1++;
 			}
 		}
 		System.out.println("총"+totalStudents+"명 중"+ cnt1+"등");
+		
+		
+//      번외) 모든 학생의 등수 구하기
+//		- 등수를 10개 구해야 핟나(변수 10개 or 배열1개)
+		int[] rank = new int[] {
+			1, 1, 1, 1, 1, 1, 1, 1, 1, 1,	
+		};
+
+//      등수 계산
+		
+		for(int i=0; i < data.length; i++) {   //for(int i=0; i <= 9; i++)
+			for(int k=0; k < data.length;k++) {
+				if(data[i] < data[k]) {   //0~9까지 반복
+					rank[i]++;
+				}
+	     	}
+		
+		}
+		
+//      등수 출력
+		for(int i=0; i<rank.length; i++) {
+			System.out.println(rank[i]);
+			System.out.println(" ");
+		}
+		
+		
+		
+		
 		
 	}
 
