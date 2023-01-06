8008c08ab7098931ee860daeea4e200d481a3b20
CHOONSIM <yoyyo9402@gmail.com>
2023-01-06 18:06:50 +0900

    패키지 변경(휴대폰요금)

---
+++ Day08/src/oop/test5/CellphoneFee.java
@@ -0,0 +1,28 @@
+package oop.test5;
+
+public class CellphoneFee {
+	
+	String company, name;
+	int monthfee, data, callmin, text;
+	
+	void setting (String company, String name, int monthfee, int data, int callmin, int text) {
+		this.company = company;
+		this.name = name;
+		this.monthfee = monthfee;
+		this.data = data;
+		this.callmin = callmin;
+		this.text = text;
+	}
+	
+	void output () {
+		System.out.println("<요금제 정보>");
+		System.out.println("통신사 : "+ this.company);
+		System.out.println("이름 : "+ this.name);
+		System.out.println("월정액요금 : "+ this.monthfee);
+		System.out.println("월 데이터 : "+ this.data);
+		System.out.println("월 통화량 : "+ this.callmin);
+		System.out.println("월 문자량 : "+ this.text);
+		System.out.println();
+		}
+
+}
---
+++ Day08/src/oop/test5/Test01.java
@@ -0,0 +1,24 @@
+package oop.test5;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		CellphoneFee c1 = new CellphoneFee();
+		CellphoneFee c2 = new CellphoneFee();
+		CellphoneFee c3 = new CellphoneFee();
+		
+		c1.setting("SK", "5G언택트52", 52000, 200, 1000, 2000);
+		c2.setting("KT", "5G세이브  ", 45000, 100, 900, 1500);
+		c3.setting("LG", "5G시그니처  ", 130000, 500, 2000, 2500); 
+		
+		System.out.println();
+		
+		c1.output();
+		System.out.println();
+		c2.output();
+		System.out.println();
+		c3.output();
+		
+	}
+
+}
