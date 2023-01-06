4734e4ab2bd4510566633e82b50844aa14ff84c9
CHOONSIM <yoyyo9402@gmail.com>
2023-01-06 18:04:46 +0900

    월드컵 득점순위 문제 풀이

---
+++ Day08/src/oop/test2/Test01.java
@@ -0,0 +1,78 @@
+package oop.test2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+	worldcup w1 = new worldcup();
+	worldcup w2 = new worldcup();
+	worldcup w3 = new worldcup();
+	worldcup w4 = new worldcup();
+	
+	
+	w1.num = 1;
+	w1.name = "킬리안 음바페";
+	w1.country = "프랑스";
+	w1.goal = 8;
+	
+	
+	w2.num = 2;
+	w2.name = "리오넬 메시";
+	w2.country = "아르헨티나";
+	w2.goal = 7;
+
+	
+	w3.num = 3;
+	w3.name = "올리비에 지루";
+	w3.country = "프랑스";
+	w3.goal = 4;
+
+	
+	w4.num = 4;
+	w4.name = "훌리안 알바레스";
+	w4.country = "아르헨티나";
+	w4.goal = 4;
+	
+	
+	System.out.print(w1.num);
+	System.out.print("\t");
+	System.out.print(w1.name);
+	System.out.print("\t");
+	System.out.print(w1.country);
+	System.out.print("\t");
+	System.out.print(w1.goal);
+	
+	System.out.println();
+	
+	System.out.print(w2.num);
+	System.out.print("\t");
+	System.out.print(w2.name);
+	System.out.print("\t");
+	System.out.print(w2.country);
+	System.out.print("\t");
+	System.out.print(w2.goal);
+	
+	System.out.println();
+	
+	System.out.print(w3.num);
+	System.out.print("\t");
+	System.out.print(w3.name);
+	System.out.print("\t");
+	System.out.print(w3.country);
+	System.out.print("\t");
+	System.out.print(w3.goal);
+	
+	System.out.println();
+	
+	System.out.print(w4.num);
+	System.out.print("\t");
+	System.out.print(w4.name);
+	System.out.print("\t");
+	System.out.print(w4.country);
+	System.out.print("\t");
+	System.out.print(w4.goal);
+	
+	}
+	
+	
+
+}
---
+++ Day08/src/oop/test2/worldcup.java
@@ -0,0 +1,10 @@
+package oop.test2;
+
+public class worldcup {
+	
+	int num;
+	String name;
+	String country;
+	int goal;
+
+}
