ab8ffee9bf5dd3b61d70589affe2a2478dd79a8e
CHOONSIM <yoyyo9402@gmail.com>
2023-01-09 11:04:42 +0900

    생성자(constructor)

---
+++ Day09/src/oop/constructor01/Test01.java
@@ -0,0 +1,16 @@
+package oop.constructor01;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		//Student s1 = new Student();
+		
+		Student s1 = new Student("피카츄", 70);
+	
+		
+		//s1.setting("피카츄", 70);
+		
+		s1.output();
+	}
+
+}
