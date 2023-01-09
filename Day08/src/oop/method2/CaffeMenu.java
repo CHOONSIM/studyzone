334a26bf5690c411e7b28ac9a065540266fb6335
CHOONSIM <yoyyo9402@gmail.com>
2023-01-09 09:31:05 +0900

    과제 내풀이(카페메뉴)

--- Day08/src/oop/method2/CaffeMenu.java
+++ Day08/src/oop/method2/CaffeMenu.java
@@ -1,5 +1,31 @@
 package oop.method2;
 
 public class CaffeMenu {
-
+	
+	String menu, category ,sale;
+	int price; boolean Dc; int discount = 20;
+	
+	void setting(String menu, String category, int price, 
+			boolean Dc) {
+		this.menu = menu;
+		this.category = category;
+		this.price = price;
+		this.Dc = Dc;
+	}
+	void output() {
+		System.out.println("<메뉴 정보>");
+		if(this.Dc == true) {
+			System.out.println("메뉴 : " + this.menu + " 행사중");
+			System.out.println("분류 : " + this.category);
+			System.out.println("가격 : " + this.price *(100-discount)/100 + "원");
+			System.out.println("행사여부 : 행사중");
+		}
+		else {
+			System.out.println("메뉴 : " + this.menu);
+			System.out.println("분류 : " + this.category);
+			System.out.println("가격 : " + this.price +"원");
+			System.out.println("행사여부 : - ");
+		}
+		System.out.println();
+	}
 }
--- Day08/src/oop/method2/CaffeMenuTest.java
+++ Day08/src/oop/method2/CaffeMenuTest.java
@@ -1,5 +1,26 @@
 package oop.method2;
 
 public class CaffeMenuTest {
+	public static void main(String[] args) {
+		
+		   
+		
+		CaffeMenu m1 = new CaffeMenu();
+		CaffeMenu m2 = new CaffeMenu();
+		CaffeMenu m3 = new CaffeMenu();
+		CaffeMenu m4 = new CaffeMenu();
+		
+		m1.setting("아메리카노", "음료", 2500, true);
+		m2.setting("모카라떼", "음료", 3500, false);
+		m3.setting("치즈케이크", "디저트", 5000, true);
+		m4.setting("마카롱", "디저트", 3000, false);
+		
+		
+		m1.output();
+		m2.output();
+		m3.output();
+		m4.output();
+			
+		}
 
-}
+	}
