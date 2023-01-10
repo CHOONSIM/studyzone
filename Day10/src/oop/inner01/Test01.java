fca0ecf6110e5eade5ddd09ea192c8db5f00d49b
CHOONSIM <yoyyo9402@gmail.com>
2023-01-10 10:21:02 +0900

    자유로운 클래스 구조의 문제점

--- Day10/src/oop/inner01/Test01.java
+++ Day10/src/oop/inner01/Test01.java
@@ -1,5 +1,14 @@
 package oop.inner01;
 
 public class Test01 {
+	public static void main(String[] args) {
+		//경찰생성
+		Police p = new Police();
+		
+		p.setName("포돌이");
+		p.setGun(new Gun());
+		
+	}
+	
 
 }
--- Day10/src/oop/inner01/Thief.java
+++ Day10/src/oop/inner01/Thief.java
@@ -1,5 +1,20 @@
 package oop.inner01;
 
 public class Thief {
+	private String name;  //도둑은 이름을 가질 수 있다.
+	private Gun gun;      //도둑은 권총을 가질 수 있다?
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Gun getGun() {
+		return gun;
+	}
+	public void setGun(Gun gun) {
+		this.gun = gun;
+	}
 
 }
