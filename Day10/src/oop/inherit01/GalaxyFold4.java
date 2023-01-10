cf68374418697457479436d8c978c8ebfbd26888
CHOONSIM <yoyyo9402@gmail.com>
2023-01-10 16:48:09 +0900

    상속의 필요성

--- Day10/src/oop/inherit01/GalaxyFold4.java
+++ Day10/src/oop/inherit01/GalaxyFold4.java
@@ -1,5 +1,19 @@
 package oop.inherit01;
 
 public class GalaxyFold4 {
+	//필드
+	private String number;
+	private String color;
+	
+	//메소드
+	public void call() {
+		System.out.println("전화");
+	}
+	public void sms() {
+		System.out.println("문자");
+	}
+	public void samsungPay() {
+		System.out.println("삼성페이");
+	}
 
 }
