0b688cb81268baabf0b92e3cd1d7faec5873907a
CHOONSIM <yoyyo9402@gmail.com>
2023-01-10 14:06:55 +0900

    static 활용 예제 두 번째

---
+++ Day10/src/oop/keyword06/Robot.java
@@ -0,0 +1,9 @@
+package oop.keyword06;
+
+public class Robot {
+	
+	//static이 없다면 객체를 만들어야함, 일회성 계산인데 항상 객체를 생성해야하는 불필요함을 없앰
+	public static int plus(int left, int right) {
+		return left + right;
+	}
+}
---
+++ Day10/src/oop/keyword06/Test01.java
@@ -0,0 +1,12 @@
+package oop.keyword06;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//아주 간단한 계산물 객체지향 방식으로 구현
+		// - ex : 10 + 20 (10,20 : 피연산자)
+		
+		int a = Robot.plus(10, 20);
+		System.out.println("a = " + a);
+	}
+
+}
