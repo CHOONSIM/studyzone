0f38ed3136f972e7297fc086c51164f102388082
CHOONSIM <yoyyo9402@gmail.com>
2023-01-12 17:04:37 +0900

    이름 검사 문제

---
+++ Day12/src/api/lang/string/Test07.java
@@ -0,0 +1,27 @@
+package api.lang.string;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Test07 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이름을 입력하세요");
+		String name = sc.nextLine();
+		sc.close();
+		
+		
+		if (!(Pattern.matches("^[가-힣]*$", name))){
+			System.out.println("이름에 사용 가능한 문자를 사용해주세요");
+		}
+		else if(!(name.length() >= 2 && name.length() <=7)) {
+			System.out.println("글자수가 잘못입력 되었습니다");
+		}
+		else {
+			System.out.println("이름이 설정되었습니다.");
+		}
+		
+	}
+
+}
