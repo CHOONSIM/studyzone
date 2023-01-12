c61b7280f909e825c9924d049ffbbf6b8fcbacb5
CHOONSIM <yoyyo9402@gmail.com>
2023-01-12 18:16:42 +0900

    문자열 편집

--- Day12/src/api/lang/string/Test01.java
+++ Day12/src/api/lang/string/Test01.java
@@ -18,7 +18,7 @@ public class Test01 {
 		//글자수
 		System.out.println(a.length());
 		System.out.println(b.length());
-		
+		 
 		//비교
 		System.out.println(a == b); //동일비교(의미없음)  (포장지가 같냐)
 		System.out.println(a.equals(b)); //동등비교(글자가 같은지 비교)   (내용물이 같냐)
