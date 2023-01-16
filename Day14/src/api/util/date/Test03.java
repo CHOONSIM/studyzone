5ce9eca6caacaaeae920f8c30deb284eb4a7bbb8
CHOONSIM <yoyyo9402@gmail.com>
2023-01-16 11:07:08 +0900

    Date 출력 문제 풀이

---
+++ Day14/src/api/util/date/Test03.java
@@ -0,0 +1,37 @@
+package api.util.date;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		Date a = new Date();
+		
+		
+//		SimpleDateFormat b = new SimpleDateFormat("yyyy년\sMM월\sdd일\sE");
+//		System.out.println(b.format(a));
+		
+//		오버캐스팅
+		Format b = new SimpleDateFormat("yyyy년 MM월 dd일 E");
+		System.out.println(b.format(a));
+	
+		
+		Format c = new SimpleDateFormat("a H시 m분");
+		System.out.println(c.format(a));
+		
+//		LocalTime d = LocalTime.now();
+//		System.out.println(d.getHour() + ":" + d.getMinute() +":" + d.getSecond());
+		Format d = new SimpleDateFormat("HH:mm:ss");
+		System.out.println(d.format(a));
+		
+		
+		Format e = new SimpleDateFormat("yyyy-MM-dd E HH:mm:ss");
+		System.out.println(e.format(a));
+
+	}
+
+}
