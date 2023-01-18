7238eeb93037a863c9f2ebc4a9abf22cf1de554d
CHOONSIM <yoyyo9402@gmail.com>
2023-01-18 18:05:39 +0900

    파일 분석

---
+++ Day16/src/api/io/single/Test01.java
@@ -0,0 +1,19 @@
+package api.io.single;
+
+import java.io.File;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+//		목표 : single byte 단위 데이터 출력(저장)
+//		준비물 : 파일 객체(없으면 생김) , 스트림 객체(출력용 통로)
+		
+//		도구 준비
+		File target = new File("temp", "single.kh");
+		
+		
+
+	}
+
+}
