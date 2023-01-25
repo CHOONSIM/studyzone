b4814a50a32ba9bb3e52a38559b63ffc66ad5106
CHOONSIM <yoyyo9402@gmail.com>
2023-01-25 18:20:34 +0900

    Spring JDBC INSERT 예제

--- jdbc01/src/jdbc/test/Test01.java
+++ jdbc01/src/jdbc/test/Test01.java
@@ -23,11 +23,11 @@ public class Test01 {
 		
 		dataSource.setUsername("kh11");		//계정이름
 		dataSource.setPassword("kh11"); 	//계정비밀번호
-		dataSource.setDriverClassName("oracle.jdbc.OracleDriver"); 		//오라크 드라이버 정보(내가 쓸 데이터베이스 종류)
-		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");		//DB주소 등록
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver"); 	//오라클 드라이버 정보(내가 쓸 데이터베이스 종류)
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");	//DB주소 등록 (localhost IP주소)
 		
 		//명령 실행 도구 생성
-		JdbcTemplate jdbcTemplate = new JdbcTemplate();		//jdbcTemplate 쏘는도구
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();		//jdbcTemplate 전송도구
 		jdbcTemplate.setDataSource(dataSource);
 		
 		System.out.println("도구 준비 완료");
