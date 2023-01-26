c6fb17641a9244b1bbec4523f96bebfaad35bc78
CHOONSIM <yoyyo9402@gmail.com>
2023-01-26 12:54:29 +0900

    subject, student 수정 예제

---
+++ jdbc02/src/update/Test03.java
@@ -0,0 +1,34 @@
+package update;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.util.JdbcUtils;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		//데이터
+		int no = 40;
+		String name = "자바초급과정";
+		int period = 60;
+		int price = 800000;
+		String type = "오프라인";
+		
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		
+		String sql = "update subject set name=?, period=?, price=?, type=? where no=?";
+		Object[] param = {name, period, price, type, no};
+		
+		int count = jdbcTemplate.update(sql, param);
+		
+		if(count > 0) {
+			System.out.println("수정완료");
+		}
+		else {
+			System.out.println("대상이 없음");
+		}
+		
+	}
+
+}
---
+++ jdbc02/src/update/Test04.java
@@ -0,0 +1,34 @@
+package update;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.util.JdbcUtils;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		
+		//데이터
+		int no = 2;
+		String name = "아무개2";
+		int korean = 100;
+		int english = 100;
+		int math = 10;
+		
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		
+		String sql = "update student set name=?, korean=?, english=?, math=? where no=?";
+		Object[] param = {name, korean, english, math, no};
+		
+		int count = jdbcTemplate.update(sql, param);
+		
+		if(count > 0) {
+			System.out.println("수정완료");
+		}
+		else {
+			System.out.println("실행대상이 없음");
+		}
+
+	}
+
+}
