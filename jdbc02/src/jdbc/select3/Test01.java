8f878e41c1ccf9ac1f77cf0098f3148da6ad615d
CHOONSIM <yoyyo9402@gmail.com>
2023-01-26 17:51:36 +0900

    단일 조회 예제

--- jdbc02/src/jdbc/select3/Test01.java
+++ jdbc02/src/jdbc/select3/Test01.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import dto.PocketmonDto;
+import jdbc.dto.PocketmonDto;
 import jdbc.util.JdbcUtils;
 
 public class Test01 {
