487f8f2d8b02480e7e027511d214fe0e05bd7191
CHOONSIM <yoyyo9402@gmail.com>
2023-01-26 17:51:26 +0900

    student 조회 풀이

--- jdbc02/src/jdbc/select/Test04.java
+++ jdbc02/src/jdbc/select/Test04.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import dto.StudentDto;
+import jdbc.dto.StudentDto;
 import jdbc.util.JdbcUtils;
 
 public class Test04 {
