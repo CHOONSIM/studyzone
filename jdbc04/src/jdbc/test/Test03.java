a7afcfe0eecb42b6bfb8371fef1e3b8a29b7232a
CHOONSIM <yoyyo9402@gmail.com>
2023-01-27 16:50:21 +0900

    Subject - 삭제

---
+++ jdbc04/src/jdbc/test/Test03.java
@@ -0,0 +1,23 @@
+package jdbc.test;
+
+import jdbc.dao.SubjectDao;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+//		삭제
+		int no = 1;
+		
+		SubjectDao dao = new SubjectDao();
+		boolean result = dao.delete(no);
+		
+		if(result) {
+			System.out.println("삭제완료");
+		}
+		else {
+			System.out.println("삭제할 대상이 없음");
+		}
+	}
+
+}
