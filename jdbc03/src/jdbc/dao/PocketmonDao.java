ae92a0cfe63552cf65ba345babc80960ceca5db3
CHOONSIM <yoyyo9402@gmail.com>
2023-01-27 14:50:12 +0900

    모듈화 - 목록,검색,상세,수정,삭제

--- jdbc03/src/jdbc/dao/PocketmonDao.java
+++ jdbc03/src/jdbc/dao/PocketmonDao.java
@@ -2,6 +2,7 @@ package jdbc.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,7 +29,7 @@ public class PocketmonDao {
 	
 	
 	
-//	  C(등록) - insert into pocketmon(no,name,type) values(?,?,?)
+//	  C(등록)
 //		public void insert(int no, String name, String type)
 		public void insert(PocketmonDto dto) {
 			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
@@ -42,17 +43,53 @@ public class PocketmonDao {
 			jdbcTemplate.update(sql,param);
 		}
 
-//	  R(목록)
+//	  R(목록) 
+		public List<PocketmonDto> selectList() {
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			String sql = "select*from pocketmon order by no asc";
+//			List<PocketmonDto> list = jdbcTemplate.query(sql, mapper);
+//			return list;		줄임 ↓
+			return jdbcTemplate.query(sql, mapper);
+		}
+//	  R(검색)
+		public List<PocketmonDto> selectList(String column, String keyword){
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			String sql = "select * from pocketmon where instr(#1,?) > 0 order by #1 asc";
+			sql = sql.replace("#1", column);
+			Object[] param = {keyword};
+			return jdbcTemplate.query(sql, mapper, param);
+		}
 //	  R(상세)
-//	  U(수정) - update pocketmon set name =?, type=? where no =?
-		public boolean update(PocketmonDto dto) {		//boolean, 수정이 되었는지 안되었는지
+		public PocketmonDto selectOne(int no) {
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			String sql = "select * from pocketmon where no = ?";
+			Object[] param = {no};
+			List<PocketmonDto> list = jdbcTemplate.query(sql, mapper, param);
+			if(list.isEmpty()) {
+				return null;
+			}
+			else {
+				return list.get(0);
+			}
+		}
+		
+//	  U(수정)
+		public boolean update(PocketmonDto dto) {		//반환형 boolean, 수정이 되었는지 안되었는지
 			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
 			String sql = "update pocketmon set name =?, type=? where no =?";
 			Object[] param = {dto.getName(), dto.getType(), dto.getNo()};
 			int result = jdbcTemplate.update(sql,param);
 			return result > 0;
-
-//	  D(삭제)
-
 		}
+
+//	  D(삭제) - delete pocketmon where no = ?		?=int
+		public boolean delete(int no) {					//반환형 boolean,삭제가 될수있고 안될수있음
+			JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+			String sql = "delete pocketmon where no = ?";
+			Object[] param = {no};
+			
+			int result = jdbcTemplate.update(sql, param);
+			return result > 0;
+		}
+
 }
