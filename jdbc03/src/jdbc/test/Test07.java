d4c3535fe0a8ec9d9452f692875372a8b491ace1
CHOONSIM <yoyyo9402@gmail.com>
2023-01-27 14:49:05 +0900

    Pocketmon - 상세

---
+++ jdbc03/src/jdbc/test/Test07.java
@@ -0,0 +1,28 @@
+package jdbc.test;
+
+import jdbc.dao.PocketmonDao;
+import jdbc.dto.PocketmonDto;
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		
+//		Pocketmon 상세조회
+		
+//		데이터 준비
+		int no = 1;
+		
+//		JDBC
+		PocketmonDao dao = new PocketmonDao();
+		PocketmonDto dto = dao.selectOne(no);
+		
+		if(dto == null) {
+			System.out.println("검색한 번호의 내용이 없습니다");
+		}
+		else {
+			System.out.println(dto);
+		}
+
+	}
+
+}
