816f7b835fede15968d823a9cd0e82cc7b594466
CHOONSIM <yoyyo9402@gmail.com>
2023-01-30 16:41:28 +0900

    파라미터 처리용 애노테이션

---
+++ spring06parameter/src/main/java/com/kh/spring06/controller/ParameterController.java
@@ -0,0 +1,27 @@
+package com.kh.spring06.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ParameterController {
+	
+//	주소에 ?를 적고 뒤에 ket=value 형태로 데이터를 추가할 수 있다.
+//	이를 요청 파라미터(request parameter)라고 부른다.
+//	서버에서는 파라미터의 이름과 형태를 반드시 알아야 한다.
+//	@RequestParam과 변수를 사용하여 파라미터를 수신하도록 선언
+	
+	@RequestMapping("/age")
+	@ResponseBody
+	public String age(
+//			@RequestParam(required = false) String name,		- null 가능 
+			@RequestParam String name, 							//	- null 불가
+			@RequestParam int birth) {
+//		System.out.println("name = " + name);
+//		System.out.println("birth = " + birth);
+		int result = 2023 - birth +1;
+		return name + "님의 나이는 " + result + "입니다.";
+	}
+}
