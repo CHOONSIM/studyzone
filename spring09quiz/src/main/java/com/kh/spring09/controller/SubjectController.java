eef16b41e347a16043d9c06f7e4b7a20388e9dd6
CHOONSIM <yoyyo9402@gmail.com>
2023-01-31 17:49:39 +0900

    Spring 09 quiz - Controller 생성

--- spring09quiz/src/main/java/com/kh/spring09/controller/SubjectController.java
+++ spring09quiz/src/main/java/com/kh/spring09/controller/SubjectController.java
@@ -21,15 +21,26 @@ public class SubjectController {
 //	@RequestMapping("/detail")
 //	@RequestMapping("/edit")
 //	@RequestMapping("/delete")
+	
 //		등록
 	@Autowired
 	private SubjectDao dao;
 	
+	
+//	@RequestMapping("/insert")
+//	@ResponseBody
+//	public String insert(@ModelAttribute SubjectDto dto) {
+//		dao.insert(dto);
+//		return"등록완료";		// -> 목록으로 이동 
+//	}
+	
+//	등록이 완료된 이후에 텍스트 메세지가 아니라 다른곳으로 보내고 싶다면
+//	사용자에게 재접속을 요청하도록 처리할 수 있다(Redirect, 리다이렉트)
 	@RequestMapping("/insert")
-	@ResponseBody
 	public String insert(@ModelAttribute SubjectDto dto) {
 		dao.insert(dto);
-		return"등록완료";
+//		return "redirect:list";					//상대경로
+		return"redirect:/subject/list";		//절대경로	
 	}
 	
 //		목록검색
@@ -70,10 +81,12 @@ public class SubjectController {
 	@ResponseBody
 	public String edit(@ModelAttribute SubjectDto dto) {
 		boolean done = dao.update(dto);
-		if(done)
-			return"변경작업 완료";
-		else
-			return"변경작업 대상 없음";
+		
+		return done  ? "변경작업 완료" : "변견작업 대상 없음"; 		//삼항연산자 - 조건 ? if : else
+//		if(done)
+//			return"변경작업 완료";
+//		else
+//			return"변경작업 대상 없음";
 		}
 		
 //		삭제
