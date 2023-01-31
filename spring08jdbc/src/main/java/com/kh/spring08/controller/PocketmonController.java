a3373c4329d3ff96d0e95b456f0358c9539bb6c3
CHOONSIM <yoyyo9402@gmail.com>
2023-01-31 13:19:39 +0900

    Pocketmon Controller - 등록,목록&검색,수정,삭제,상세

--- spring08jdbc/src/main/java/com/kh/spring08/controller/PocketmonController.java
+++ spring08jdbc/src/main/java/com/kh/spring08/controller/PocketmonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.spring08.dao.PocketmonDao;
@@ -37,7 +38,7 @@ public class PocketmonController {
 	}
 	
 	
-	//	목록
+//		목록
 	@RequestMapping("/list")
 	@ResponseBody
 	public String list() {
@@ -51,4 +52,50 @@ public class PocketmonController {
 		}
 		return buffer.toString();
 	}
+	
+//		검색
+	@RequestMapping("/serch")
+	@ResponseBody
+	public String search(@RequestParam String column, 
+								@RequestParam String keyword) {
+		List<PocketmonDto> list = dao.selectList(column, keyword);
+		
+		StringBuffer buffer = new StringBuffer();
+		for(PocketmonDto dto : list) {
+			buffer.append(dto.toString());
+			buffer.append("<br>");				//줄바꿈				
+		
+		}
+		return buffer.toString();
+	}
+	
+	
+//		목록&검색 통합페이지
+	@RequestMapping("/all-in-one")		//주소에는 대시(-) 사용가능
+	@ResponseBody
+	public String allInOne(
+			@RequestParam(required = false, defaultValue = "name") String column,
+			@RequestParam(required = false, defaultValue = "") String keyword) {
+			boolean search = !keyword.equals("");
+			
+			List<PocketmonDto> list;
+			if(search) {
+				list = dao.selectList(column, keyword);
+			}
+			else {
+				list = dao.selectList();
+			}
+			StringBuffer buffer = new StringBuffer();
+			for(PocketmonDto dto : list) {
+				buffer.append(dto.toString());
+				buffer.append("<br>");				//줄바꿈				
+			
+			}
+			return buffer.toString();
+	}
+	
+	
+	
+	
+	
 }
