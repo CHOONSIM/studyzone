e8ff5e0202c8769f0742535c7978306a324bbacb
CHOONSIM <yoyyo9402@gmail.com>
2023-01-31 09:45:41 +0900

    파라미터 처리용 애노테이션

--- spring06parameter/src/main/java/com/kh/spring06/controller/ParameterController.java
+++ spring06parameter/src/main/java/com/kh/spring06/controller/ParameterController.java
@@ -10,7 +10,7 @@ public class ParameterController {
 	
 //	주소에 ?를 적고 뒤에 key=value 형태로 데이터를 추가할 수 있다.
 //	이를 요청 파라미터(request parameter)라고 부른다.
-//	서버에서는 파라미터의 이름과 형태를 반드시 알아야 한다.
+//	서버에서는 파라미터의 이름과 형태를 반드시 알아야 한다. -> 서버가 정하고 거기에 따른다
 //	@RequestParam과 변수를 사용하여 파라미터를 수신하도록 선언
 	
 	@RequestMapping("/age")
