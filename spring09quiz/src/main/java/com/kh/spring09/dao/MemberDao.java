ff93ff46ccf600df8c3c9a5f9e7ba30fe1efd51e
CHOONSIM <yoyyo9402@gmail.com>
2023-02-01 12:41:38 +0900

    Spring09 member 과제

--- spring09quiz/src/main/java/com/kh/spring09/dao/MemberDao.java
+++ spring09quiz/src/main/java/com/kh/spring09/dao/MemberDao.java
@@ -44,10 +44,10 @@ public class MemberDao {
 //		등록
 	public void newbie(MemberDto dto) {
 		String sql = "insert into member(member_id, member_pw, member_nick, member_tel, member_email, member_birth, member_post, "
-				+ "member_basic_addr, member_detail_addr, member_level, member_point, member_join) "
+				+ "member_basic_addr, member_detail_addr, member_level, member_point, member_join, member_login) "
 				+ "values(?,?,?,?,?,?,?,?,?,'준회원',0,sysdate,null)";
 		Object[] param = {dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(), dto.getMemberTel(), dto.getMemberEmail(), dto.getMemberBirth(), dto.getMemberPost(), 
-				dto.getMemberBasicAddr(), dto.getMemberDetailAddr(), dto.getMemberLevel(), dto.getMemberPoint(), dto.getMemberJoin()};
+				dto.getMemberBasicAddr(), dto.getMemberDetailAddr(), dto.getMemberLevel(), dto.getMemberPoint(), dto.getMemberJoin(), dto.getMemberLogin()};
 		jdbcTemplate.update(sql,param);
 	}
 	
