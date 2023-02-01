5eba40c3a8efcc5aa751a2f2e85333feab91c8e3
CHOONSIM <yoyyo9402@gmail.com>
2023-02-01 13:16:24 +0900

    Spring09 과제

--- spring09quiz/src/main/java/com/kh/spring09/dao/MemberDao.java
+++ spring09quiz/src/main/java/com/kh/spring09/dao/MemberDao.java
@@ -22,32 +22,32 @@ public class MemberDao {
 
 		@Override
 		public MemberDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-			MemberDto dto = new MemberDto();
-			dto.setMemberId(rs.getString("member_id"));
-			dto.setMemberPw(rs.getString("member_pw"));
-			dto.setMemberNick(rs.getString("member_nick"));
-			dto.setMemberTel(rs.getString("member_tel"));
-			dto.setMemberEmail(rs.getString("member_email"));
-			dto.setMemberBirth(rs.getString("member_birth"));
-			dto.setMemberPost(rs.getString("member_post"));
-			dto.setMemberBasicAddr(rs.getString("member_basic_addr"));
-			dto.setMemberDetailAddr(rs.getString("member_detail_addr"));
-			dto.setMemberLevel(rs.getString("member_level"));
-			dto.setMemberPoint(rs.getInt("member_point"));
-			dto.setMemberJoin(rs.getDate("member_join"));
-			dto.setMemberLogin(rs.getDate("member_login"));
-			return dto;
+			MemberDto memberDto = new MemberDto();
+			memberDto.setMemberId(rs.getString("member_id"));
+			memberDto.setMemberPw(rs.getString("member_pw"));
+			memberDto.setMemberNick(rs.getString("member_nick"));
+			memberDto.setMemberTel(rs.getString("member_tel"));
+			memberDto.setMemberEmail(rs.getString("member_email"));
+			memberDto.setMemberBirth(rs.getString("member_birth"));
+			memberDto.setMemberPost(rs.getString("member_post"));
+			memberDto.setMemberBasicAddr(rs.getString("member_basic_addr"));
+			memberDto.setMemberDetailAddr(rs.getString("member_detail_addr"));
+			memberDto.setMemberLevel(rs.getString("member_level"));
+			memberDto.setMemberPoint(rs.getInt("member_point"));
+			memberDto.setMemberJoin(rs.getDate("member_join"));
+			memberDto.setMemberLogin(rs.getDate("member_login"));
+			return memberDto;
 		}
 		
 	};
 	
 //		등록
-	public void newbie(MemberDto dto) {
+	public void newbie(MemberDto memberDto) {
 		String sql = "insert into member(member_id, member_pw, member_nick, member_tel, member_email, member_birth, member_post, "
 				+ "member_basic_addr, member_detail_addr, member_level, member_point, member_join, member_login) "
 				+ "values(?,?,?,?,?,?,?,?,?,'준회원',0,sysdate,null)";
-		Object[] param = {dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(), dto.getMemberTel(), dto.getMemberEmail(), dto.getMemberBirth(), dto.getMemberPost(), 
-				dto.getMemberBasicAddr(), dto.getMemberDetailAddr(), dto.getMemberLevel(), dto.getMemberPoint(), dto.getMemberJoin(), dto.getMemberLogin()};
+		Object[] param = {memberDto.getMemberId(), memberDto.getMemberPw(), memberDto.getMemberNick(), memberDto.getMemberTel(), memberDto.getMemberEmail(), memberDto.getMemberBirth(), memberDto.getMemberPost(), 
+				memberDto.getMemberBasicAddr(), memberDto.getMemberDetailAddr()};
 		jdbcTemplate.update(sql,param);
 	}
 	
@@ -74,11 +74,11 @@ public class MemberDao {
 	}
 		
 //		수정
-	public boolean update(MemberDto dto) {
+	public boolean update(MemberDto memberDto) {
 		String sql = "update member set (member_id, member_pw, member_nick, member_tel, member_email, member_birth, member_post, "
-				+ "member_basic_addr, member_detail_addr, member_level, member_point, member_join";
-		Object[] param = {dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(), dto.getMemberEmail(), dto.getMemberBirth(), dto.getMemberPost(), 
-				dto.getMemberBasicAddr(), dto.getMemberDetailAddr(), dto.getMemberLevel(), dto.getMemberPoint(), dto.getMemberJoin(), dto.getMemberLogin()};
+				+ "member_basic_addr, member_detail_addr, member_level, member_point, member_join, member_login";
+		Object[] param = {memberDto.getMemberId(), memberDto.getMemberPw(), memberDto.getMemberNick(), memberDto.getMemberTel(), memberDto.getMemberEmail(), memberDto.getMemberBirth(), memberDto.getMemberPost(), 
+				memberDto.getMemberBasicAddr(), memberDto.getMemberDetailAddr()};
 		return jdbcTemplate.update(sql, param) > 0;
 	}
 	
