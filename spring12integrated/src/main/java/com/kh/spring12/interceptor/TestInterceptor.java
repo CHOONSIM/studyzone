85d9b979e988a1845cb1d0650207dadc7a7b2a11
CHOONSIM <yoyyo9402@gmail.com>
2023-02-09 17:03:12 +0900

    테스트용 인터셉터 생성 및 등록

---
+++ spring12integrated/src/main/java/com/kh/spring12/interceptor/TestInterceptor.java
@@ -0,0 +1,34 @@
+package com.kh.spring12.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/*
+ * 인터셉터(Interceptor)
+ * - 스프링의 실행 과정에 개입하여 변화를 일으킬 수 있는 도구
+ * - 등록만으로 사용할 수 없음
+ * - 상속을 통한 자격 획득 및 스프링에 사용 설정을 해야 함
+ * - HandlerInterceptor 상속
+ * 	- 다음 메소드를 재정의하여 사용
+ * 		- preHandle - 컨트롤러가 실해되기 직전 시점에 간섭
+ * 		- postHandle
+ * 		- afterCompletion
+ */
+@Service
+
+public class TestInterceptor implements HandlerInterceptor {
+	public boolean preHandle(
+			HttpServletRequest request, 		//요청 정보가 담긴 객체
+			HttpServletResponse response,	//응답 정보가 담긴 객체
+			Object handler)						//실행될 대상 컨트롤러 정보
+		throws Exception{
+//		인터셉터는 컨트롤러처럼 선언한다고 주지 않는다.
+//		- HttpSession은 HttpServletRequest 객체 내부에 포함되어 있다.
+		System.out.println("테스트입터셉터");
+//		return false;	//차단
+		return true;
+	}
+}
