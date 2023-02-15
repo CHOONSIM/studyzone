17544df22431e3a72aa074adbe890328abd1a8dd
CHOONSIM <yoyyo9402@gmail.com>
2023-02-15 15:19:11 +0900

    페이징 클래스 계산

--- spring12integrated/src/main/java/com/kh/spring12/vo/PaginationVO.java
+++ spring12integrated/src/main/java/com/kh/spring12/vo/PaginationVO.java
@@ -14,6 +14,7 @@ public class PaginationVO {
 	private int page = 1;
 	private int size = 10;
 	private int count;
+	private int blockSize = 10;
 	
 //	시작행번호 계산
 	public int getBegin() {
@@ -24,5 +25,51 @@ public class PaginationVO {
 	public int getEnd() {
 //		return page*size;
 		return Math.min(page*size,  count);
+	}
+	
+//	전체 페이지 수
+	public int getTotalPage() {
+		return (count + size -1)/size;
+	}
+	
+//	시작블록번호
+	public int getStartBlock() {
+		int value = (page-1) / blockSize*blockSize +1;
+		return Math.min(value, getTotalPage());
+	}
+	
+//	종료블록번호
+	public int getFinishBlock() {
+		return (page-1) / blockSize*blockSize + blockSize;
+	}
+	
+//	첫 페이지인지
+	public boolean isFirst() {
+		return page == 1;
+	}
+	
+//	마지막 페이지인지
+	public boolean isLast() {
+		return page == getTotalPage();
+	}
+	
+//	[이전]이 나오는 조건 : 시작블록이 1보다 크면 (page가 size보다 크면)
+	public boolean isPreview() {
+		return getStartBlock() > 1;
+	}
+	
+//	[다음]이 나오는 조건 : 종료블록이 마지막 페이지보다 작으면
+	public boolean isNext() {
+		return getFinishBlock() < getTotalPage();
+	}
+	
+//	[이전]을 누르면 나올 페이지 번호
+	public int getPrevPage() {
+		return getStartBlock()-1;
+	}
+	
+//	[다음]을 누르면 나올 페이지 번호
+	public int getNextPage() {
+		return getStartBlock()+1;
 	}
 }
--- spring12integrated/src/test/java/com/kh/spring12/PagingTest2.java
+++ spring12integrated/src/test/java/com/kh/spring12/PagingTest2.java
@@ -20,7 +20,12 @@ public class PagingTest2 {
 		vo.setSize(10);
 		vo.setCount(1200);
 		
-		System.out.println(vo.getBegin());		// 시작행번호
-		System.out.println(vo.getEnd());			// 종료행번호
+		System.out.println("시작행번호 =" + vo.getBegin());						// 시작행번호
+		System.out.println("종료행번호 =" + vo.getEnd());							// 종료행번호
+		System.out.println("전체 페이지 수	=" + vo.getTotalPage());			// 전체 페이지 수	
+		System.out.println("시작블록번호 =" + vo.getStartBlock());			// 시작블록번호
+		System.out.println("종료블록번호 =" + vo.getFinishBlock());			// 종료블록번호
+		System.out.println("첫 페이지 =" + vo.isFirst());	
+		System.out.println("마지막 페이지 =" + vo.isLast());	
 	}
 }
