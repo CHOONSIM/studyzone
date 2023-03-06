aabc4e47d2348a8eadd89b3422977ef4cf1b258d
CHOONSIM <yoyyo9402@gmail.com>
2023-03-06 10:34:33 +0900

    외부 자바스크립트 파일 불러오기

--- css/javascript/test04.js
+++ css/javascript/test04.js
@@ -1,5 +1,5 @@
 //  자바스크립트 코드를 가질 수 있는 파일
- 
+  
 document.querySelector("button")            // 버튼을 찾아서
                 .addEventListener("click", function(){  // 클릭하면 실행
             alert("Hello");
