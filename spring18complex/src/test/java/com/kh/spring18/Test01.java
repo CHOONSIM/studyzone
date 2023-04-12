5d751db0c5de2f1634af832371421bd18f67f7b8
CHOONSIM <yoyyo9402@gmail.com>
2023-04-12 14:17:29 +0900

    복합 검색 구현(member)

--- spring18complex/src/test/java/com/kh/spring18/Test01.java
+++ spring18complex/src/test/java/com/kh/spring18/Test01.java
@@ -31,7 +31,18 @@ public class Test01 {
 	@Test
 	public void test2() {
 		Map<String, Object>param = new HashMap<>();
-		param.put("memberId","user");
+//		param.put("memberId","user");
+//		param.put("memberNick", "유저");
+//		param.put("memberLevelList", List.of("일반회원","우수회원","관리자"));
+//		param.put("memberEmail", "kh.com");
+//		param.put("memberBirth", "2000");
+//		param.put("memberTel", "01055550169");
+//		param.put("memberAddress", "서울시");
+//		param.put("minPoint", "100");
+//		param.put("maxPoint", "10000");
+		param.put("beginJoinDate", "2023-02-08");
+		param.put("endJoinDate", "2023-02-09");
+//		param.put("searchLoginDays", 30);
 		List<MemberDto>list=sqlSession.selectList("member.complexSearch", param);
 		log.debug("개수={}",list.size());
 //		for(MemberDto dto : list) {
