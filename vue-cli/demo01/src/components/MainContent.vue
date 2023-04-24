afb8e74b0a68724d6ef689602758054448f70d81
CHOONSIM <yoyyo9402@gmail.com>
2023-04-24 13:17:54 +0900

    Vue Component 생성 및 스타일 설정, props 설명

---
+++ vue-cli/demo01/src/components/MainContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contents">
+      <h1>VueJS로 만든 홈페이지</h1>
+      <img src="https://via.placeholder.com/700x400">
+    </div>
+</template>
+
+<script>
+export default {
+    //name
+};
+</script>
+
+<style>
+</style>
