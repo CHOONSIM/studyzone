6b42606ea90f006a7edc78c229a1bc642edfc009
CHOONSIM <yoyyo9402@gmail.com>
2023-04-24 15:46:54 +0900

    Router Link 설정

--- vue-cli/demo02/src/components/MainHeader.vue
+++ vue-cli/demo02/src/components/MainHeader.vue
@@ -2,14 +2,24 @@
   <div class="header">
       <h1>{{ title }}</h1>
 
+      <!--
+        메뉴 구현
+          - 메뉴를 누르면 <RouterView/>가 변해야 한다
+          - 일반적인 a태그로는 연결하기가 어렵다(Hash여부를 알 수 없음)
+          - <RouterView/> 에 연결하기 위해서는 <RouterLink/>를 사용한다
+            - to 속성에 이동할 주소를 작성
+            - / 가 절대경로 시작점을 의미
+            - v-bind가 붙으면 동적으로 설정 가능
+      -->
       <!-- 메뉴 구현 -->
-      <a href="#">홈</a>
+
+      <RouterLink to="/">홈</RouterLink>
       &nbsp;
-      <a href="#">게시판</a>
+      <RouterLink to="/pocketmon">포켓몬</RouterLink>
       &nbsp;
-      <a href="#">로그인</a>
+      <RouterLink to="/subject">강의정보</RouterLink>
       &nbsp;
-      <a href="#">정보</a>
+      <RouterLink to="/info">정보</RouterLink>
   </div>
 </template>
 
