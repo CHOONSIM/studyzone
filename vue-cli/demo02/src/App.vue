0d4ed5a851861ca1ebac837690b4e4919e6b1d42
CHOONSIM <yoyyo9402@gmail.com>
2023-04-24 13:21:07 +0900

    최초 프로젝트 생성

---
+++ vue-cli/demo02/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- 헤더 -->
+    <!-- <MainHeader title="KH정보교육원 프론트엔드 수업자료"/> -->
+    <!-- <MainHeader v-bind:title="title"/> -->
+    <MainHeader :title="title"/>
+
+    <!-- 컨텐츠 -->
+    <MainContent/>
+
+    <!-- 푸터 -->
+    <MainFooter/>
+    
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+import MainContent from './components/MainContent.vue';
+import MainFooter from './components/MainFooter.vue';
+export default {
+  //components에서는 사용할 화면 조각(vue)를 정의한다
+  components:{
+    MainHeader, 
+    MainContent, 
+    MainFooter
+  },
+  //data에서는 현재 컴포넌트에서 사용할 데이터를 정의한다
+  data(){
+    return {
+      title:"KH정보교육원 프론트엔드 수업자료",
+    };
+  },
+};
+</script>
+
+<!-- 
+    style을 사용할 때 전체스타일과 컴포넌트 스타일을 구분할 수 있다 
+    - <style> 로 만들면 전체 스타일이 된다
+    - <style scoped> 로 만들면 컴포넌트 전용 스타일이 된다
+-->
+<style>
+</style>
---
+++ vue-cli/demo02/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+/*
+    CDN 방식일 때는
+    Vue.createApp({}).mount("#app");과 같이 작성했음
+
+    모듈 방식에서
+    -> import {createApp} from 'vue' 코드를 이용하여
+
+    createApp 함수만 별도로 불러왔기 때문에 다음과 같은코드가 가능
+    -> createApp({}).mount('#app)
+
+    App.vue에서 export default로 내보내진 객체를 불러와서
+    -> import App from './App.vue'
+
+    createApp에 객체로 설정하겠다
+    -> createApp(App).mount('#app')
+*/
