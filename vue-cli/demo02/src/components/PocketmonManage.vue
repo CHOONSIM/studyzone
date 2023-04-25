3120fdb27ab0d7270a3ec4618534218f341df6d3
CHOONSIM <yoyyo9402@gmail.com>
2023-04-25 11:05:26 +0900

    axios interceptor 구현

--- vue-cli/demo02/src/components/PocketmonManage.vue
+++ vue-cli/demo02/src/components/PocketmonManage.vue
@@ -11,9 +11,24 @@
                         </div>
                     </div>
                 </div>
+                <div class="row mt-4">
+                    <div class="col-sm-6 mb-4"
+                    v-for="(pocketmon) in pocketmonList"
+                    :key="pocketmon.no">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ pocketmon.name }}</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">{{pocketmon.type}}</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
-                <div class="row">
-                    <div class="col">
+                <div class="row mt-4">
+                    <div class="col-sm-6 mb-4">
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
@@ -32,6 +47,7 @@
                         </table>
                     </div>
                 </div>
+                
 
             </div>
         </div>
@@ -40,8 +56,9 @@
 
 <script>
 // 이 파일에서만 Axios를 사용하고 싶다면 이곳에서 import를 진행하면 된다
-import axios from "axios"; 
-console.log(axios);
+// - axios에 추가적인 설정을 해야하는 경우가 있다면 매 파일마다 해야함
+
+//import axios from "axios";
 
     export default{
         data(){
@@ -53,7 +70,8 @@ console.log(axios);
         methods:{
             async loadList(){
                 const url = "http://localhost:8080/pocketmon/";
-                const resp = await axios.get(url);
+                // const resp = await axios.get(url);
+                const resp = await this.$http.get(url);     // vue에 등록한 $http를 사용
                 this.pocketmonList.push(...resp.data);
             },
         },
--- vue-cli/demo02/src/main.js
+++ vue-cli/demo02/src/main.js
@@ -15,6 +15,15 @@ import {router} from './utils/router'
 // import {router} from './utils/router/index.js'
 
 // 애플리케이션 생성 코드
+const app = createApp(App);
+
+// axios 모듈을 전체에서 사용할 수 있도록 Global 사용설정(vue Global Properties)
+import {axios} from "./utils/axios";
+// app.config.globalProperties["$http"] = axios;
+app.config.globalProperties.$http = axios;
+//this.$http 로 부르면 아무곳에서 import없이 axios사용가능
+
 // - 라우터를 사용하도록 설정해야함(use 함수 사용)
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+app.use(router).mount('#app');
  
