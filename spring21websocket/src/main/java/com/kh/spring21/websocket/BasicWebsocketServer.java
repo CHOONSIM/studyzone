1f622d514cce4739bb2699c907a511da439b7851
CHOONSIM <yoyyo9402@gmail.com>
2023-04-27 15:28:53 +0900

    기본 웹소켓 예제 서버구현 및 등록

---
+++ spring21websocket/src/main/java/com/kh/spring21/websocket/BasicWebsocketServer.java
@@ -0,0 +1,39 @@
+package com.kh.spring21.websocket;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+  스프링에서 웹소켓을 처리하는 서버 
+  1 컨트롤러로 접속해서 HTTP연결이 이루어지고 난 뒤 WS 연결 생성
+  2 만약 페이지를 이탈할 경우 자동으로 WS연결이 종료(수동종료도 가능)
+  3 아무나 서버가 될 수 없음, 자격 획득을 위해 상속을 받음(상속받을때는 구체적일수록 좋음)
+  	- interface WebsocketHandler
+  	- class TextWebSocketHandler(텍스트), BinaryWebSocketHandler(파일)
+  4 스프링에 등록한다(Configuration의 @Bean 또는 @Service)
+  5 필요한 메소드를 재정의하여 원하는 코드를 작성
+ 	- afterConnectionEstablished : 연결 이루어진 경우 자동으로 실행
+ 		- WebSocketSession : 웹소켓 연결된 대상의 정보(HttpSession 아님)
+ 		
+ 	- afterConnectionClosed : 연결이 해제된 경우 자동으로 실행
+ 		- WebSocketSession : 웹소켓 해제된 대상의 정보(HttpSession 아님)
+ 		- CloseStatus : 연결이 해제된 이유에 대한 상태 코드
+ */
+@Slf4j
+@Service
+public class BasicWebsocketServer extends TextWebSocketHandler{
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.debug("사용자 접속");
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		log.debug("사용자 종료");
+	}
+}
---
+++ spring21websocket/src/main/java/com/kh/spring21/websocket/WebSocketServerConfiguration.java
@@ -0,0 +1,29 @@
+package com.kh.spring21.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+//웹소켓 사용 설정 파일
+@Configuration
+@EnableWebSocket		//웹소켓 사용 설정 활성화
+public class WebSocketServerConfiguration implements WebSocketConfigurer{
+
+	@Autowired
+	private BasicWebsocketServer basicWebsocketServer;
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//		basicWebsocketServer 를 어딘가에 등록하겠다
+//		만들어둔 웹소켓 서버를 등록하는 코드를 작성
+//		-주의-
+//		- 어떠한 주소와도 겹치면 안됨(비어있는 페이지로 설정)
+		
+//		BasicWebsocketServer를 ws://localhost:8080/ws/basic 주소에서 사용하겠습니다.
+		registry.addHandler(basicWebsocketServer, "/ws/basic");
+		
+	}
+
+}
