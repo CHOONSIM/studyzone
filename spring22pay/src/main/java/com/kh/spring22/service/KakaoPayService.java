026bd724c4055d5c1c5f6412fda99bc763eebfdc
CHOONSIM <yoyyo9402@gmail.com>
2023-05-02 16:03:25 +0900

    카카오페이 준비 서비스 구현

---
+++ spring22pay/src/main/java/com/kh/spring22/service/KakaoPayService.java
@@ -0,0 +1,16 @@
+package com.kh.spring22.service;
+
+import java.net.URISyntaxException;
+
+import com.kh.spring22.vo.KakaoPayReadyRequestVO;
+import com.kh.spring22.vo.KakaoPayReadyResponseVO;
+
+public interface KakaoPayService {
+
+	// 준비 - ready
+//	카카오페이 결제 준비 응답데이터 ready(카카오페이 결제 준비 요청데이터);
+	KakaoPayReadyResponseVO ready (KakaoPayReadyRequestVO vo) throws URISyntaxException;
+	
+	// 승인 - approve
+//	KakaoPayApproveResponseVO approve(KakaoPayApproveRequestVO vo);
+}
---
+++ spring22pay/src/test/java/com/kh/spring22/Test04.java
@@ -0,0 +1,42 @@
+package com.kh.spring22;
+
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring22.service.KakaoPayService;
+import com.kh.spring22.vo.KakaoPayReadyRequestVO;
+import com.kh.spring22.vo.KakaoPayReadyResponseVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+// 목표 : KakaoPayService를 사용하여 결제 준비 테스트
+@Slf4j
+@SpringBootTest
+public class Test04 {
+
+	@Autowired
+	private KakaoPayService kakaoPayService;
+	
+	@Test
+	public void test() throws URISyntaxException {
+		//데이터 준비
+		KakaoPayReadyRequestVO vo = new KakaoPayReadyRequestVO();
+		vo.setPartner_order_id( UUID.randomUUID().toString());
+		vo.setPartner_user_id( UUID.randomUUID().toString());
+		vo.setItem_name("용산 아파트");
+		vo.setQuantity(1);
+		vo.setTotal_amount(10000000);
+		
+		//처리
+		KakaoPayReadyResponseVO response = kakaoPayService.ready(vo);
+		
+		//출력
+		log.debug("request = {}", vo);
+		log.debug("response = {}", response);
+		
+	}
+}
