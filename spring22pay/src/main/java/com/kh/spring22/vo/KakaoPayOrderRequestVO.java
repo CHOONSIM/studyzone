0eb616989e6da3dd6f2c30dbde3fab75462c000b
CHOONSIM <yoyyo9402@gmail.com>
2023-05-03 17:52:39 +0900

    조회 - order 구현중

--- spring22pay/src/main/java/com/kh/spring22/vo/KakaoPayOrderRequestVO.java
+++ spring22pay/src/main/java/com/kh/spring22/vo/KakaoPayOrderRequestVO.java
@@ -1,5 +1,17 @@
 package com.kh.spring22.vo;
 
-public class KakaoPayOrderRequestVO {
+import lombok.Data;
 
+@Data
+public class KakaoPayOrderRequestVO {
+	
+    private String partner_order_id; // 가맹점 주문번호
+    private String partner_user_id; // 가맹점 회원 id
+    private String item_name; // 상품명
+    private int quantity; // 상품 수량
+    private int total_amount; // 상품 총액
+    private int tax_free_amount; // 상품 비과세 금액
+    private String approval_url; // 결제 성공시 redirect url
+    private String cancel_url; // 결제 취소시 redirect url
+    private String fail_url; // 결제 실패시 redirect url
 }
--- spring22pay/src/main/java/com/kh/spring22/vo/KakaoPayOrderResponseVO.java
+++ spring22pay/src/main/java/com/kh/spring22/vo/KakaoPayOrderResponseVO.java
@@ -1,5 +1,11 @@
 package com.kh.spring22.vo;
 
+import lombok.Data;
+
+@Data
 public class KakaoPayOrderResponseVO {
 
+    private String tid; // 결제 고유 번호
+    private String next_redirect_pc_url; // PC 결제를 위한 URL
+    private String created_at; // 결제 준비 요청 시간
 }
