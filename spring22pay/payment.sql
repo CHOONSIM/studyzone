b5c6f9ea17eca65401dbacbec53c4a0bb1427cf4
CHOONSIM <yoyyo9402@gmail.com>
2023-05-03 15:49:42 +0900

    대표 결제 정보 테이블

---
+++ spring22pay/payment.sql
@@ -0,0 +1,13 @@
+--대표 결제 테이블(결제1건에 대한 정보)
+create table payment(
+payment_no number primary key,
+payment_tid varchar2(20) not null,
+payment_name varchar2(300) not null,
+payment_total number not null,
+payment_remain number not null,
+payment_time date sysdate not null,
+member_id varchar2(20) not null
+);
+
+select*from table payment;
+delete table payment;
